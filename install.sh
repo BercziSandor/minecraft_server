91935d0d5a5dd1a60d5a468a48537eef0df1590b
Berczi Sandor <berczi.sandor@gmail.com>
2021-01-17 15:01:22 +0100

    Adding vanilla

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+java_memory_options="-Xms1G -Xmx1G"
 
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -13,7 +14,7 @@ op_system=win
 
 # Parameters
 mode=$1
-mode=${mode:-paper}
+mode=${mode:-vanilla}
 echo "Mode:    [$mode]"
 
 version=$2
@@ -24,15 +25,22 @@ if [ "$mode" == "forge" ]; then
 	# version=${version:-1.16.5-36.0.0}
 	installerLink=https://files.minecraftforge.net/maven/net/minecraftforge/forge/${version}-${rev}/forge-${version}-${rev}-installer.jar
 	version=${version}-${rev}
+
 elif [ "$mode" == "paper" ]; then
 	version=1.16.5
 	rev=428
 	installerLink=https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${rev}/downloads/paper-${version}-${rev}.jar
 	version=${version}-${rev}
+
+elif [ "$mode" == "vanilla" ]; then
+	# See: https://www.minecraft.net/en-us/download/server/
+	version=1.16.5
+	installerLink=https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 else
-	echo "TODO 23645, aborting"
+	echo "Invalid mode: $mode, aborting"
 	exit 1
 fi
+
 echo "Version: [$version]"
 echo
 
@@ -47,22 +55,28 @@ if [ -e "$outDir" ]; then
 fi
 mkdir -p "$outDir"
 
+
+
+cd "$outDir" >/dev/null
 if [ "$mode" == "forge" ]; then
-	cd "$outDir" >/dev/null
-	curl --output 	installer.jar   $installerLink
+	curl --output 	installer.jar			$installerLink
 	java -jar 		installer.jar --installServer
 	rm installer.jar
-	javaParams="-Xms1G -Xmx1G -jar forge-${version}.jar nogui pause"
+	javaParams="$java_memory_options -jar forge-${version}.jar nogui pause"
 
 elif [ "$mode" == "paper" ]; then
-	cd "$outDir" >/dev/null
-	curl --output 	paper-${version}.jar   $installerLink
-	javaParams="-Xms1G -Xmx1G -jar paper-${version}.jar nogui pause"
+	curl --output 	${mode}-${version}.jar	$installerLink
+	javaParams="$java_memory_options -jar paper-${version}.jar nogui pause"
 
 elif [ "$mode" == "vanilla" ]; then
-	echo "TODO 23s545, aborting"
+	curl --output 	${mode}-${version}.jar	$installerLink
+	javaParams="$java_memory_options -jar ${mode}-${version}.jar nogui pause"
+else
 	exit 1
 fi
+
+
+
 
 cd $SCRIPTDIR
 echo "Creating start script..."
@@ -80,8 +94,15 @@ else
 fi
 
 echo "Starting the server first time..."
-cmd.exe /c $startScript
+if [ $op_system == 'win' ]; then
+	cmd.exe /c $startScript
+else
+	echo "TODO 23545, aborting"
+	exit 1
+fi
 
 #  eula
-echo eula=true> $outDir/eula.txt
 
+echo eula=true> $outDir/eula.txt
+echo
+echo "The server ir ready to start. Script: $SCRIPTDIR/$startScript"
