2a04336b448fc3160f030ed8e0a8240f1013fa03
Berczi Sandor <berczi.sandor@gmail.com>
2021-01-18 08:47:45 +0100

    forge fix

--- install.sh
+++ install.sh
@@ -8,29 +8,34 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPTDIR
 
 op_system=win
-# Forge: http://files.minecraftforge.net/
-# Paper: https://papermc.io/
 
 
 # Parameters
 mode=$1
-mode=${mode:-vanilla}
+mode=${mode:-forge}
 echo "Mode:    [$mode]"
 
 version=$2
 if [ "$mode" == "forge" ]; then
-	version=1.16.4
-	rel=35.1.4
+	version=1.16.5
+	rev=36.0.0
 	# version=${version:-1.16.4-35.1.4}
 	# version=${version:-1.16.5-36.0.0}
+	echo "Forge site: http://files.minecraftforge.net/"
 	installerLink=https://files.minecraftforge.net/maven/net/minecraftforge/forge/${version}-${rev}/forge-${version}-${rev}-installer.jar
 	version=${version}-${rev}
 
 elif [ "$mode" == "paper" ]; then
 	version=1.16.5
 	rev=428
+	echo "Paper site: https://papermc.io/"
 	installerLink=https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${rev}/downloads/paper-${version}-${rev}.jar
 	version=${version}-${rev}
+
+elif [ "$mode" == "spigot" ]; then
+	version=1.16.5
+	echo "Spigot site: https://www.spigotmc.org"
+	installerLink=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 
 elif [ "$mode" == "vanilla" ]; then
 	# See: https://www.minecraft.net/en-us/download/server/
@@ -59,24 +64,21 @@ mkdir -p "$outDir"
 
 cd "$outDir" >/dev/null
 if [ "$mode" == "forge" ]; then
-	curl --output 	installer.jar			$installerLink
+	curl --output 	installer.jar $installerLink
 	java -jar 		installer.jar --installServer
 	rm installer.jar
-	javaParams="$java_memory_options -jar forge-${version}.jar nogui pause"
-
 elif [ "$mode" == "paper" ]; then
-	curl --output 	${mode}-${version}.jar	$installerLink
-	javaParams="$java_memory_options -jar paper-${version}.jar nogui pause"
-
+	curl --output 	${mode}-${version}.jar $installerLink
 elif [ "$mode" == "vanilla" ]; then
-	curl --output 	${mode}-${version}.jar	$installerLink
-	javaParams="$java_memory_options -jar ${mode}-${version}.jar nogui pause"
+	curl --output 	${mode}-${version}.jar $installerLink
+elif [ "$mode" == "spigot" ]; then
+	curl --output 	BuildTools.jar $installerLink
+	# git config --global --unset core.autocrlf
+	java -jar 		BuildTools.jar --rev $version
 else
 	exit 1
 fi
-
-
-
+javaParams="$java_memory_options -jar ${mode}-${version}.jar nogui pause"
 
 cd $SCRIPTDIR
 echo "Creating start script..."
@@ -105,4 +107,4 @@ fi
 
 echo eula=true> $outDir/eula.txt
 echo
-echo "The server ir ready to start. Script: $SCRIPTDIR/$startScript"
+echo "The server is ready to start. Script: $SCRIPTDIR/$startScript"
