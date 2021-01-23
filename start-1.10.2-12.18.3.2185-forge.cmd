127e04c7dd5556b01d77259fc7a03cc4ec8a4d9a
Berczi Sandor <berczi.sandor@gmail.com>
2021-01-23 17:55:07 +0100

    fd

--- start-1.10.2-12.18.3.2185-forge.cmd
+++ start-1.10.2-12.18.3.2185-forge.cmd
@@ -1,9 +1,5 @@
 @echo off
-echo *****************************
-echo The ip of the server:
 START "" /WAIT CMD /C "echo IP of the server: & curl -s ipinfo.io/ip & echo. & pause"
-echo 
-echo *****************************
 pushd %~dp0\1.10.2-12.18.3.2185-forge
  java -Xms1G -Xmx1G -jar forge-1.10.2-12.18.3.2185-universal.jar nogui pause
 popd
