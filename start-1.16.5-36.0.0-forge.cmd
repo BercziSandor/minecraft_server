fe0c7595b67524d4d954c1cf59a3a4a79fccea75
Berczi, Sándor <berczi.sandor@gmail.com>
2021-03-04 20:47:20 +0100

    d

--- start-1.16.5-36.0.0-forge.cmd
+++ start-1.16.5-36.0.0-forge.cmd
@@ -1,4 +1,5 @@
 @echo off
+START "" /WAIT CMD /C "echo IP of the server: & curl -s ipinfo.io/ip & echo. & pause"
 pushd %~dp0\1.16.5-36.0.0-forge
  java -Xms1G -Xmx1G -jar forge-1.16.5-36.0.0.jar nogui pause
 popd
